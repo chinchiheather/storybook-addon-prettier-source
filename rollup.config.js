cd83ca990c633cc1bd29dc3371d69bffa1c27d94
Heather Roberts <heather.roberts@3plearning.com>
2018-08-14 17:44:31 +1000

    Add npm script to compile addon

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  external: [
+    'prop-types',
+    'react',
+    'prettier/standalone',
+    'prettier/parser-babylon',
+    'react-syntax-highlighter/prism-light',
+    'react-syntax-highlighter/languages/prism/jsx',
+    'react-syntax-highlighter/styles/prism/tomorrow',
+    'react-element-to-string'
+  ],
+  plugins: [
+    resolve(),
+    postcss(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    })
+  ]
+};
